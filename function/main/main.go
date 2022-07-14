4fdf612bb72c85c571a12cb236549908149800c4
李欢庆 <huanqingli@yangqianguan.com>
2022-07-14 10:11:30 +0800

    包的引用

--- function/main/main.go
+++ function/main/main.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"fmt"
-	"project01/function/utils/utils"
+	"src/go_code/project01/function/utils"
 )
 
-var sum float64
+//var sum float64
 
 func main() {
 	var n1 float64 = 1
 	var n2 float64 = 2
+	var opt byte = '+'
 	a := utils.Cal(n1, n2, opt)
 	fmt.Println(a)
 }
